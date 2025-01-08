84c7277a84e6808c2686e6899328e6659620319a
ParkSY0919 <qhr498@gmail.com>
2025-01-08 18:40:37 +0900

    [Feat/#5] MapKit - 1, 2번
    식당 정보를 맵뷰 어노테이션으로 표시, 최초 뷰컨트롤러 진입 시에는 전체 어노테이션을 다 보여주기

--- DailyTask_week3/DailyTask_week3/DailyTask_week3/DailyTask250108/RestaurantViewController.swift
+++ DailyTask_week3/DailyTask_week3/DailyTask_week3/DailyTask250108/RestaurantViewController.swift
@@ -7,23 +7,68 @@
 
 import UIKit
 
+import MapKit
+
 class RestaurantViewController: UIViewController {
 
+    let restauratList = RestaurantList().restaurantArray
+    
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setRegister()
+        setNavUI()
+        configureMapView()
+    }
+    
+    func setNavUI() {
+        navigationController?.navigationBar.tintColor = .lightGray
+        let rightBtn = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(navRightBtnTapped))
+        navigationItem.rightBarButtonItem = rightBtn
+    }
+    
+    func setRegister() {
+        mapView.delegate = self
+    }
+    
+    @objc
+    func navRightBtnTapped(_ sender: UIButton) {
+        print(#function)
+        var strArr = [String]()
+        for i in restauratList {
+            strArr.append(i.name)
+        }
+        let alert = UIAlertUtil.showActionSheet(title: "장소", message: "오잉", actionArr: strArr)
+        present(alert, animated: true)
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension RestaurantViewController: MKMapViewDelegate {
+ 
+    func configureMapView() {
+        let center = CLLocationCoordinate2D(latitude: 37.65370, longitude: 127.04740)
+        
+        mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 800, longitudinalMeters: 800)
+        
+        let annotationArr = restauratList
+        
+        for i in annotationArr {
+            makeMKPointAnnotaion(name: i.name, latitude: i.latitude, longitude: i.longitude)
+        }
     }
-    */
-
+    
+    //경도, 위도, 이름
+    func makeMKPointAnnotaion(name: String, latitude: Double, longitude: Double) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        annotation.title = name
+        self.mapView.addAnnotation(annotation)
+    }
+    
 }
