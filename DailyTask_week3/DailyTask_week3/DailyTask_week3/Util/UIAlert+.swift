5a06e5f4b59d8eb720f7c54496cdc9ada856cd81
ParkSY0919 <qhr498@gmail.com>
2025-01-08 18:39:57 +0900

    [Feat/#5] feat UIAlertUtil

--- DailyTask_week3/DailyTask_week3/DailyTask_week3/Util/UIAlert+.swift
+++ DailyTask_week3/DailyTask_week3/DailyTask_week3/Util/UIAlert+.swift
@@ -5,4 +5,22 @@
 //  Created by 박신영 on 1/8/25.
 //
 
-import Foundation
+import UIKit
+
+struct UIAlertUtil {
+    
+    static func showAlert(title: String, message: String) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        return alert
+    }
+    
+    static func showActionSheet(title: String, message: String, actionArr: [String]) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for i in actionArr {
+            alert.addAction(UIAlertAction(title: i, style: .default))
+        }
+        return alert
+    }
+    
+}
