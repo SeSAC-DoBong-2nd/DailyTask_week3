d6a42d86e4c7b2f2e6352da960dbf368a1a30028
ParkSY0919 <qhr498@gmail.com>
2025-01-09 15:55:55 +0900

    [Feat/#9] add UpDownGame files & outlet property 배치

---
+++ DailyTask_week3/DailyTask_week3/DailyTask_week3/DailyTask250109/GameWaitingViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GameWaitingViewController.swift
+//  DailyTask_week3
+//
+//  Created by 박신영 on 1/9/25.
+//
+
+import UIKit
+
+class GameWaitingViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var numTextField: UITextField!
+    @IBOutlet var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
