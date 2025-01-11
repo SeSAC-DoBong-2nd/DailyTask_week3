000311e35d609eb527e3c680dcb04c815f7d71ea
ParkSY0919 <qhr498@gmail.com>
2025-01-11 21:18:45 +0900

    [Add/#14] add Chatting files

---
+++ DailyTask_week3/DailyTask_week3/DailyTask_week3/DailyTask250110/TravelTalkViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TravelTalkViewController.swift
+//  DailyTask_week3
+//
+//  Created by 박신영 on 1/11/25.
+//
+
+import UIKit
+
+class TravelTalkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
